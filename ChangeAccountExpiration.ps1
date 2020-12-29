bb25eb38f13779fbe65b1834920f7a5caa640b4a
edgelock <44417754+edgelock@users.noreply.github.com>
2020-12-29 11:15:41 -0500

    Add files via upload

---
+++ ChangeAccountExpiration.ps1
@@ -0,0 +1,11 @@
+﻿#to extend a single user's account expiration by 90 days- bbarnes is the username
+Set-ADAccountExpiration -Identity bbarnes -TimeSpan 90.0:0
+
+#to extend a single user's account out to a specific date- pattifu is the username
+Set-ADAccountExpiration -Identity PattiFu -DateTime "03/22/2021"
+
+#to change the manager of a single user- mflowers is user, ashowers is manager
+get-aduser mflowers | Set-ADUser -Manager ashowers
+
+#to get a list of direct reports a manager has, ashowers is manager
+get-aduser ashowers -Properties DirectReports | Select -Expand DirectReports | get-aduser -Properties Title | Select Name,Title
